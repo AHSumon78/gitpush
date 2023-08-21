52c3101c22951b6088b0712c8660e42097c176b0
MD. SUMON ALI <mmdsumonali@gamil.com>
2023-08-21 21:55:07 +0600

    sparse matrix

---
+++ sparseMat.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter row :";
+    cin>>x;
+    int A[x][x];
+    int B[x*(x+1)/2];
+    for(int i=0;i<x;i++){
+       for(int j=0;j<x;j++){
+             cin>>A[i][j];
+       }
+    }
+    int k=0;
+       for(int i=0;i<x;i++){
+       for(int j=0;j<x;j++){
+             if(A[i][j]!=0){
+                B[k]=A[i][j];
+                  k++;
+             }
+       }
+    }
+    int r;
+    cout<<"which row to print :";
+    cin>>r;
+    r-=1;
+    int c=r+1;
+    r=r*(r+1)/2;
+    for(int i=r;i<r+x;i++){
+             if(c>0)
+            cout<<B[i]<<" ";
+             else
+             cout<<"0 ";
+             c--;
+
+    }
+
+
+    
+}
